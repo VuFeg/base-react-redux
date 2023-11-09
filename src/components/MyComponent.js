4c14e77b0975c31ee18996c810bcfaa68d0febf4
VuFeg <ngduogtruongvu@gmail.com>
2023-11-10 00:52:34 +0700

    test nesting component

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,44 +1,11 @@
 import React from "react";
+import UserInfor from "./UserInfor";
 
 class MyComponent extends React.Component {
-    state = {
-        name: "VuFeng",
-        address: "Da Nang City",
-        age: 20
-    };
-
-    handleClick(e) {
-        console.log("Click me my button");
-
-        this.setState({
-            name: "Vu Nguyen",
-            age: Math.floor(Math.random() * 100 + 1)
-        });
-    }
-
-    handleMouseOver(e) {
-        // console.log(e.pageX);
-    }
-
-    handleOnChange = (e) => {
-        this.setState({
-            name: e.target.value
-        })
-    }
-
-    handleOnSubmit = (e) => {
-        e.preventDefault();
-        console.log(this.state)
-    }
-
     render() {
         return (
             <div>
-                My name is {this.state.name}. I'm {this.state.age}
-                <form onSubmit={(e) => this.handleOnSubmit(e)}>
-                    <input type="text" onChange={(e) => this.handleOnChange(e)} />
-                    <button>Submit</button>
-                </form>
+                <UserInfor />
             </div>
         )
     }
---
+++ src/components/UserInfor.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+class UserInfor extends React.Component {
+    state = {
+        name: "VuFeng",
+        address: "Da Nang City",
+        age: 20
+    };
+
+    handleClick(e) {
+        console.log("Click me my button");
+
+        this.setState({
+            name: "Vu Nguyen",
+            age: Math.floor(Math.random() * 100 + 1)
+        });
+    }
+
+    handleMouseOver(e) {
+        // console.log(e.pageX);
+    }
+
+    handleOnChangeName = (e) => {
+        this.setState({
+            name: e.target.value,
+            age: e.target.value
+        })
+    }
+
+    handleOnChangeAge = (e) => {
+        this.setState({
+            age: e.target.value
+        })
+    }
+
+    handleOnSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state)
+    }
+
+    render() {
+        return (
+            <div>
+                My name is {this.state.name}. I'm {this.state.age}
+                <form form onSubmit={(e) => this.handleOnSubmit(e)}>
+                    <input
+                        value={this.state.name}
+                        type="text"
+                        onChange={(e) => this.handleOnChangeName(e)}
+                    />
+                    <input
+                        value={this.state.age}
+                        type="text"
+                        onChange={(e) => this.handleOnChangeAge(e)}
+                    />
+                    <button>Submit</button>
+                </form >
+            </div>
+        )
+    }
+}
+
+export default UserInfor;
