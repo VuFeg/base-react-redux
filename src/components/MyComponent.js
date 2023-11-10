46ee9882b1dd94cec4612ded6ac4bf62972c5502
VuFeg <ngduogtruongvu@gmail.com>
2023-11-10 19:16:28 +0700

    test props

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,16 @@
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
     render() {
         return (
             <div>
                 <UserInfor />
+                <br />
+                <DisplayInfor name="Vu Nguyen" age="24" />
+                <hr />
+                <DisplayInfor name="Du de" age={50} />
             </div>
         )
     }
