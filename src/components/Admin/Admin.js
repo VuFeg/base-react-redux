f5f5ee00cc2d3260480a707cc889788756f8bc13
VuFeg <ngduogtruongvu@gmail.com>
2024-01-12 00:15:27 +0700

    design admin sidebar

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,6 +1,20 @@
-const Admin = () => {
+import SideBar from "./SideBar";
+import './Admin.scss';
+import { FaBars } from 'react-icons/fa';
+import { useState } from "react";
+
+const Admin = (props) => {
+    const [collapsed, setCollapsed] = useState(false);
     return (
-        <div>Admin page</div>
+        <div className="admin-container">
+            <div className="admin-sidebar">
+                <SideBar collapsed={collapsed} />
+            </div>
+            <div className="admin-content">
+                <FaBars onClick={() => setCollapsed(!collapsed)} />
+                admin content
+            </div>
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -23,8 +23,8 @@ root.render(
         <Route path='/' element={<App />}>
           <Route index element={<HomePage />} />
           <Route path='/Users' element={<User />} />
-          <Route path='/Admins' element={<Admin />} />
         </Route>
+        <Route path='/Admins' element={<Admin />} />
       </Routes>
     </BrowserRouter>
     {/* </React.StrictMode> */}
